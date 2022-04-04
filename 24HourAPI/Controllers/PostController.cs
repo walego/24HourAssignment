f52a0dd1aa4e5d982c87f93873be180cd51d43ae
chris pettigrew <cdpettigrew26@comcast.net>
2022-04-04 17:00:03 -0400

    PostDetail Made

--- 24HourAPI/Controllers/PostController.cs
+++ 24HourAPI/Controllers/PostController.cs
@@ -4,10 +4,42 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using _24Hour.Models;
+using _24Hour.Services;
+using Microsoft.AspNet.Identity;
 
 namespace _24HourAPI.Controllers
 {
+    [Authorize]
     public class PostController : ApiController
     {
+        //this will allow us to use out post service in our methods
+        private PostService CreatePostService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var postService = new PostService(userId);
+            return postService;
+        }
+        //Post a post
+        public IHttpActionResult Post(PostCreate post)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var service = CreatePostService();
+            if (!service.CreatePost(post))
+            {
+                return InternalServerError();
+            }
+            return Ok();
+        }
+        //Get All
+        public IHttpActionResult GetAll()
+        {
+            PostService service = CreatePostService();
+            var posts = service.GetPosts();
+            return Ok(posts);
+        }
     }
 }
