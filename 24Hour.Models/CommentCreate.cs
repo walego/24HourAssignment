975d3e61613c18b083eb86289e4e3d2d4e10ebc7
Clay Burnette <clayburnette13@gmail.com>
2022-04-04 17:53:30 -0400

    Comment All Sections Setup

---
+++ 24Hour.Models/CommentCreate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24Hour.Models
+{
+    public class CommentCreate
+    {
+        [Required]
+        [MinLength(2, ErrorMessage = "Please Enter At Least Two Characters.")]
+        [MaxLength(100, ErrorMessage ="Too Many Characters!")]
+        public string Text { get; set; }
+        [MaxLength(8000)]
+        public Guid AuthorId { get; set; }
+    }
+}
---
+++ 24Hour.Models/CommentDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24Hour.Models
+{
+    public class CommentDetail
+    {
+        public int CommentId { get; set; }
+        public string Text { get; set; }
+        public string Content { get; set; }
+    }
+}
