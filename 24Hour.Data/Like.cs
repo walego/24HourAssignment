e16f54ee6ef8c2d1b6bcb1549a7198945ee5fca8
William Lego <william.a.lego11@gmail.com>
2022-04-04 15:08:33 -0400

    Like Data Class Created

--- 24Hour.Data/Like.cs
+++ 24Hour.Data/Like.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _24Hour.Data
 {
-    internal class Like
+    public class Like
     {
+        [Key]
+        public int LikeId { get; set; }
+        [Required]
+        public Guid OwnerId { get; set; }
+        public int PostId { get; set; }
+        [ForeignKey(nameof(PostId))]
+        public virtual Post LikedPost { get; set; }
     }
 }
--- 24Hour.Data/Post.cs
+++ 24Hour.Data/Post.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace _24Hour.Data
 {
-    internal class Post
+    public class Post
     {
     }
 }
