1141d1221a1b71a474c80c0128758e8601a2d9d1
Stephen Reilly <smreilly22@gmail.com>
2022-04-05 11:19:18 -0400

    Reply added

--- 24Hour.Data/Reply.cs
+++ 24Hour.Data/Reply.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,25 @@ namespace _24Hour.Data
 {
     public class Reply
     {
+        [Key]
+        public int ReplyId { get; set; }
+
+
+
+
+        [Required]
+
+
+        public string Text { get; set; }
+
+        [Required]
+        public Guid AuthorId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Comment))]
+        public int CommentId { get; set; }
+
+        public virtual Comment Comment { get; set; }
     }
+
 }
---
+++ 24Hour.Services/ReplyService.cs
@@ -0,0 +1,84 @@
+﻿using _24Hour.Data;
+using _24Hour.Models;
+using _24HourAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24Hour.Services
+{
+    public class ReplyService
+    {
+        private readonly Guid _userId;
+
+        public ReplyService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateReply(ReplyCreate reply)
+        {
+            var entity = new Data.Reply()
+            {
+                ReplyId = reply.ReplyId,
+                Text = reply.Text,
+                AuthorId = _userId,
+                CommentId = reply.CommentId
+
+            };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Replies.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<ReplyListAll> GetReply()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Replies
+                        .Where(e => e.AuthorId == _userId)
+                        .Select(
+                        e => new ReplyListAll()
+                        {
+                            ReplyId = e.ReplyId,
+                            Text = e.Text,
+                            CommentId = e.CommentId
+                        }
+
+                        );
+                return query.ToArray();
+            }
+        }
+
+        public ReplyDetail GetReplyById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Replies
+                        .Single(e => e.ReplyId == id && e.AuthorId == _userId);
+                return
+                     new ReplyDetail
+                     {
+                         ReplyId = entity.ReplyId,
+                         Text = entity.Text,
+                         CommentId = entity.CommentId
+                     };
+            }
+        }
+
+
+
+
+
+
+    }
+}
