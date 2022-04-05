81f19c43d78e6f7fcadbd84ac5714ecc68cfb6c4
William Lego <william.a.lego11@gmail.com>
2022-04-05 09:08:58 -0400

    added initial like controller

---
+++ 24HourAPI/Controllers/LikeController.cs
@@ -0,0 +1,22 @@
+﻿using _24Hour.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace _24HourAPI.Controllers
+{
+    [Authorize]
+    public class LikeController : ApiController
+    {
+       private LikeService CreateLikeService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var noteService = new LikeService(userId);
+            return noteService;
+        }
+    }
+}
