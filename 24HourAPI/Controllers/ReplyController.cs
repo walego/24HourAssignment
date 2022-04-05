bcda757581651969ef77afe17b56de7f12507b6e
Clay Burnette <clayburnette13@gmail.com>
2022-04-05 13:52:18 -0400

    Added Stephens Section

--- 24HourAPI/Controllers/ReplyController.cs
+++ 24HourAPI/Controllers/ReplyController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using _24Hour.Models;
+using _24Hour.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +11,37 @@ namespace _24HourAPI.Controllers
 {
     public class ReplyController : ApiController
     {
+
+        public IHttpActionResult Get()
+        {
+            ReplyService replyService = CreateReplyService();
+            var reply = replyService.GetReply();
+            return Ok(reply);
+        }
+
+        private ReplyService CreateReplyService()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            ReplyService replyService = CreateReplyService();
+            var reply = replyService.GetReplyById(id);
+            return Ok(reply);
+        }
+        public IHttpActionResult Post(ReplyCreate reply)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateReplyService();
+
+            if (!service.CreateReply(reply))
+                return InternalServerError();
+
+            return Ok();
+        }
     }
 }
+
