261996448c922d59bb81e688439cdc438d29fffc
William Lego <william.a.lego11@gmail.com>
2022-04-05 08:05:53 -0400

    Created first  like service

--- 24Hour.Data/Like.cs
+++ 24Hour.Data/Like.cs
@@ -14,6 +14,7 @@ namespace _24Hour.Data
         public int LikeId { get; set; }
         [Required]
         public Guid OwnerId { get; set; }
+        [Required]
         public int PostId { get; set; }
         [ForeignKey(nameof(PostId))]
         public virtual Post LikedPost { get; set; }
---
+++ 24Hour.Models/Likes/LikeListItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24Hour.Models.Likes
+{
+    public class LikeListItem
+    {
+        public int LikeId { get; set; }
+        public int PostId { get; set; }
+    }
+}
